fb4aba4d8a09e94e735cf4f3bea8a27d32690da3
Falak <falak@hotmail.co.in>
2021-01-14 09:00:42 +0530

    Minor Changes

--- app/src/Components/TripContainer.jsx
+++ app/src/Components/TripContainer.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TripImage from '../Resources/trip.png';
+import ThumbsUp from '../Resources/like.png';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -17,7 +18,7 @@ export default function TripContainer() {
 	const classes = useStyles();
 
 	return (
-		<div className={classes.root}>
+		<div className={classes.root} width="1440px" height="750px">
 			<Paper className={classes.paper}>
 				<Grid container>
 					<Grid item xs={6}>
@@ -68,8 +69,84 @@ export default function TripContainer() {
 						</Grid>
 					</Grid>
 					<Grid item xs={6}>
-						<Grid container>
-							<div width="150px" height="50px" style={{border:"1px solid block",backgroundColor:"blue"}} />
+						<Grid
+							container
+							style={{
+								position: 'relative',
+								left: '165px',
+								fontFamily: 'Montserrat',
+								textAlign: 'left',
+								width: '500px'
+							}}
+						>
+							<Grid
+								item
+								xs={12}
+								style={{
+									color: 'transparent',
+									fontSize: '15px'
+								}}
+							>
+								whitespace
+							</Grid>
+							<Grid item xs={12}>
+								<div
+									style={{
+										float: 'right',
+										backgroundColor: '#FDFFD3',
+										marginLeft: '50px',
+										width: '160px',
+										height: '50px',
+										border: '1px solid black',
+										borderRadius: '5px',
+										boxShadow: '1px 1px 0px #00000029',
+										padding: '5px'
+									}}
+								>
+									<Grid>
+										<img
+											width="31px"
+											height="34px"
+											alt="ThumbsUp"
+											style={{ opacity: '1', marginLeft: '5px' }}
+											src={ThumbsUp}
+										/>
+										<span
+											style={{
+												textAlign: 'left',
+												position: 'relative',
+												top: '0px',
+												left: '15px',
+												textTransform: 'uppercase',
+												fontFamily: 'Montserrat',
+												fontWeight: 'bold',
+												fontSize: '18px',
+												color: '#333333'
+											}}
+										>
+											125
+											<span
+												style={{
+													textAlign: 'left',
+													fontFamily: 'Montserrat',
+													fontSize: '10px',
+													position: 'absolute',
+													top:"20px",
+													right:"-45px"
+												}}
+											>
+												RECOMMEND
+											</span>
+										</span>
+									</Grid>
+								</div>
+							</Grid>
+							<Grid item xs={12} style={{ marginTop: '50px' }}>
+								<span style={{color:"#A9B7B9",fontSize:"14px",textAlign:"14px",fontWeight:"18px",fontFamily:"Montserrat"}}>Live on</span>
+								<div>
+									
+								</div>
+							</Grid>
 						</Grid>
 					</Grid>
 				</Grid>
